69ea485aaced39dc5fab686a8b70872b97c60a79
Edward Reutlinger <ereutlinger@ctc.ch>
2021-11-26 16:11:30 +0100

    added Example40

--- CyberSecurityWebApplication/CyberSecurity3WebApplication/Controllers/HomeController.cs
+++ CyberSecurityWebApplication/CyberSecurity3WebApplication/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CyberSecurity3WebApplication.Controllers
@@ -114,6 +115,91 @@ namespace CyberSecurity3WebApplication.Controllers
             }
 
             return View();
+        }
+
+        /// <summary>
+        /// shows content of file "c:\temp\pal_module_tester_series2_assert_exceptions.log" in browser
+        /// https://localhost:44365/home/example40?path=c:\temp\pal_module_tester_series2_assert_exceptions.log
+        /// 
+        /// path contains invalid characters:
+        /// https://localhost:44365/home/example40?path=c:\temp\pal_module_tester_series2_assert_exceptions.log%20%26%26%20c:\windows\notepad.exe
+        /// 
+        /// path points to a forbidden location
+        /// https://localhost:44365/home/example40?path=c:\tempxy\bla.txt
+        /// 
+        /// path does not represent a valid file
+        /// https://localhost:44365/home/example40?path=c:\temp\pal_module_tester_series2_assert_exceptions_2_.log
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Example40()
+        {
+            var context = base.HttpContext;
+
+            HttpRequest req = context.Request;
+            string queryString = req.QueryString.Value;
+            string path = req.Query["path"].FirstOrDefault();
+
+            if (path != null)
+            {
+                if (!IsValidPath(path))
+                {
+                    return Problem(detail: "path contains invalid characters", statusCode: 400);
+                }
+                else if (!path.StartsWith("c:\\temp"))
+                {
+                    return Problem(detail: "path points to a forbidden location", statusCode: 403);
+                }
+                else if (!System.IO.File.Exists(path))
+                {
+                    return Problem(detail: "path does not represent a valid file", statusCode: 404);
+                }
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = "cmd.exe";
+
+                    StringBuilder argumentsSb = new StringBuilder();
+                    argumentsSb.Append(" /c type ");
+                    argumentsSb.Append(path);
+                    process.StartInfo.Arguments = argumentsSb.ToString();
+
+
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+
+                    StreamReader reader = process.StandardOutput;
+                    string output = reader.ReadToEnd();
+                    StreamReader errorReader = process.StandardError;
+                    string error = errorReader.ReadToEnd();
+
+                    process.WaitForExit();
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine(output);
+                    sb.AppendLine(error);
+
+                    return Content(sb.ToString());
+                }
+            }
+            else
+            {
+                return Content("command parameter is missing");
+            }
+        }
+
+        /// <summary>
+        /// https://exceptionshub.com/c-sanitize-file-name.html
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static bool IsValidPath(string path)
+        {
+            Match match = Regex.Match(path, "([0-9\\-A-Za-z:\\\\_\\.]+)");
+            bool isValid = match.Success && match.Value == path;
+            return isValid;
         }
 
         public IActionResult Privacy()
