3192a6abbd349b1ad1d74db70c76ace383d8d20b
Edward Reutlinger <ereutlinger@ctc.ch>
2021-11-29 11:30:35 +0100

    SchoolController.index: read/write students

--- CyberSecurityWebApplication/CyberSecurity3WebApplication/Controllers/SchoolController.cs
+++ CyberSecurityWebApplication/CyberSecurity3WebApplication/Controllers/SchoolController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CyberSecurity3WebApplication.DataModel;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,20 @@ namespace CyberSecurity3WebApplication.Controllers
 {
     public class SchoolController : Controller
     {
+        private SchoolContext schoolContext;
+
+        public SchoolController(SchoolContext schoolContext)
+        {
+            this.schoolContext = schoolContext;
+        }
+
         public IActionResult Index()
         {
+            var students = schoolContext.Students.ToList();
+
+            // schoolContext.Students.Add(new Student { LastName = "x", FirstMidName = "y" });
+            // int cnt = schoolContext.SaveChanges();
+
             return View();
         }
     }
