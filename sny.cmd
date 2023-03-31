3d1f6e581836cd7d50eb74df1daf941c6f961530
Edward Reutlinger <ereutlinger@ctc.ch>
2023-03-31 13:32:30 +0200

    added synk.cmd

---
+++ sny.cmd
@@ -0,0 +1,32 @@
+
+
+# The token you will need to use for snyk auth is the one in your account settings here, under Auth Token: 
+# https://app.snyk.io/account
+# snyk auth <token>
+
+# token is save here
+# C:\Users\ereutlinger\.config\configstore\snyk.json
+
+
+cd C:\Work\PackageDependencyCheckerTest\OblivionAngularWebAppJavaScriptReference
+snyk monitor
+
+cd C:\Work\PackageDependencyCheckerTest\OblivionAppPackageConfigNet48
+snyk monitor
+
+
+cd C:\Work\PackageDependencyCheckerTest\OblivionAppPackageReferenceDotNet60
+snyk monitor
+
+
+cd C:\Work\PackageDependencyCheckerTest\OblivionAppPackageReferenceNet48
+snyk monitor
+
+
+cd C:\Work\PackageDependencyCheckerTest\OblivionAppPackageReferenceNodeJs
+snyk monitor
+
+
+
+
+
