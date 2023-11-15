783aa4be204502701c5b557a1b26e3a01cfb1f28
Edward Reutlinger <ereutlinger@ctc.ch>
2023-11-15 09:13:15 +0100

    tried coverlet, but generated report is empty

---
+++ CyberSecurityWebApplication/CyberSecurity3WebApplication/UnitTests/HomeController_Test.cs
@@ -0,0 +1,24 @@
+﻿using CyberSecurity3WebApplication.Controllers;
+using NUnit.Framework;
+
+namespace CyberSecurity3WebApplication.UnitTests
+{
+    [TestFixture]
+    public class HomeController_test
+    {
+        [Test]
+        public void Test1()
+        {
+            HomeController homeController = new HomeController(null);
+
+            homeController.Example10();
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+        }
+    }
+}
